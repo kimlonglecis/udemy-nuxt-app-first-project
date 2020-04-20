a14f3d457c51de3831c3f32cc815c12be472f894
long le <kimlonglecis@gmail.com>
2020-04-19 23:18:29 -0700

    minor changes

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <TheHeader @sidenavToggle="displaysidenav = !displaysidenav"/>
+    <TheSidenav
+      :show="displaysidenav"
+      @close="displaysidenav = false"
+    />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import TheHeader from "@/components/Navigation/TheHeader"
+import TheSidenav from "@/components/Navigation/TheSidenav"
+
+export default {
+  components: {
+    TheHeader,
+    TheSidenav
+  },
+  data() {
+    return {
+      displaysidenav: false
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  body
+</style>
---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="single-post-page">
+    <section class="post">
+      <h1>Title of the post</h1>
+      <div class="post-details">
+        <div>Last updated on XXX</div>
+        <div>writte by Name</div>
+      </div>
+      <p>content of the post</p>
+    </section>
+    <section class="post-feedback">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate doloremque aut non, eum quo modi minus? Deleniti quisquam consequatur unde doloremque saepe non deserunt. Eum ad incidunt a voluptatem eaque?</p>
+    </section>
+  </div>
+</template>
